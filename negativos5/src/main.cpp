5e850696d06857bae164d3e0ed65fbf0031bdefb
Gabriel <gabriel.albuquerque.129@ufrn.edu.br>
2022-04-12 14:28:10 -0300

    Update main.cpp
    Questão 1 da Lista 1 - LP1

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 using std::cin;
 using std::cout;
+using std::endl;
 
 const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int cont = 0, x, i;
+    for (i=0;i<5;i++){
+        cin >> x;
+        if(x<0){
+            cont++;
+        }
+    }
+    cout<<cont<<endl;
 
     return 0;
 }
