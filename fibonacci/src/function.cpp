affcf320375aae833d414bd7ce526d0b6d01ffc2
Gabriel <gabriel.albuquerque.129@ufrn.edu.br>
2022-04-17 14:16:26 -0300

    Update function.cpp
    Questão 4 da Lista 1 - LP1

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,25 @@
 #include "function.h"
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
+    int n1 = 0, n2 = 1, fib_next = 0;
+    cout  << "{" << n2;
+    fib_next = n1 + n2;
+    while(fib_next < n) {
+        cout<< "; " << fib_next ;
+        n1 = n2;
+        n2 = fib_next;
+        fib_next = n1 + n2;
+    }
+    cout  << "}" << endl;
     return std::vector<unsigned int>{};
+}
+int main(){
+  int n;
+    cin >> n;
+  fib_below_n(n);
 }
