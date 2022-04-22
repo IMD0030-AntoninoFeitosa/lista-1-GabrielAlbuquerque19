a1d5a67f5ff6a943a0dc749f432134a99e101c62
Gabriel <39315393+GabrielAlbuquerque19@users.noreply.github.com>
2022-04-22 18:01:24 +0000

    Questão 8 Lista 1 - LP1 (Feita)

--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -21,7 +21,23 @@ using std::max;
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int p1, p2, p3, p4, x, y;
+    while(cin >> p1 >> p2 >> p3 >> p4 >> x >> y){
+      if(p1 != p3 || p2 != p4){
+        Ponto P (x,y);
+        Ponto IE (p1, p2);
+        Ponto SD (p3, p4);
+        location_t localizacao;
+        localizacao = pt_in_rect(IE, SD, P);
+        switch(localizacao){
+          case location_t::OUTSIDE: cout << "outside" << endl; break;
+          case location_t::INSIDE: cout << "inside" << endl; break;
+          case location_t::BORDER: cout << "border" << endl; break;
+        }
+      } else {
+        cout << "invalid" << endl;
+      }
+    }
 
     return 0;
 }
